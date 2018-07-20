ca2a45ff721eac139c30dc4af8e76e33cff08e4b
guieenoutis <guieenoutis@gmail.com>
2018-07-20 15:08:09 +0900

    end setting

--- practice/babel_webpack_starter/src/app.js
+++ practice/babel_webpack_starter/src/app.js
@@ -1,3 +1,16 @@
-let test = () => console.log('HELL WORLD !!!')
+import { person, sayHello } from './lib'
 
-test()
+console.log(person.name)
+
+console.log(sayHello('seung'))
+
+async function getPosts() {
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts')
+
+  const data = await response.json()
+  return data
+}
+
+getPosts().then(post => console.log(post))
+// regeneratorRuntime is not defined
+// -> babel-polyfill to get async / await working
